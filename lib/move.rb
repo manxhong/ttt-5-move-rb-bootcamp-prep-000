a291b5adbe57fe2064cdc8779cdd3a343095f0e0
Man Hong Lee <manhong323@gmail.com>
2017-10-28 22:02:08 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board="")
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -7,3 +7,4 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+display_board();
